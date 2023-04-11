e0fbe1c3b7aa1af636d9bc289eb72f1e20426a68
Yahve6668 <95110637+Yahve6668@users.noreply.github.com>
2023-04-11 16:18:58 -0600

    v2
    v2

--- user.js
+++ user.js
@@ -1,20 +1,32 @@
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+      $btnSignUp = document.querySelector('.sign-up-btn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
 
-document.addEventListener('click',e=> {
-    let btnSignIn=document.querySelector('.sign-up-btn'),
-        btnSignUp=document.querySelector('.sign-in-btn'),
-        signUp=document.querySelector('.sign-up'),
-        signIn= document.querySelector('.sign-in');
-        console.log(signUp);
-  if(e.target==btnSignIn||e.target==btnSignUp){
-    signIn.classList.toggle('active'),
-    signUp.classList.toggle('active');
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
     }
-})
+});
 
 function openNav(){
-    document.getElementById("mobile-menu").style.width = "100%";
+  document.getElementById("mobile-menu").style.width = "100%";
 }
 
 function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%";
+  document.getElementById("mobile-menu").style.width = "0%";
+}
+
+function f(){
+  console.log("sjdnjasndnaj");
+}
+function s(){
+    console.log("sjdnjasndnaj");
+    const audio = document.createElement("audio");
+    audio.preload = "auto";
+    audio.src = "ataque.mp3";
+    audio.play();
+    document.body.appendChild(audio);
+ 
 }
